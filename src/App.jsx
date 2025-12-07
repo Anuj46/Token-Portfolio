a8d50e89644448fcfbc119e8f821fc21aa728967
Anuj Naruka <anuj.naruka.4646@gmail.com>
2025-12-07 08:58:04 +0530

    small fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 
 function App() {
   const [count, setCount] = useState(0);
